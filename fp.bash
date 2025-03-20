b08d145d07996603a214c3cc46ee898f2b4016dc
Ted Lilley <ted.lilley@digi.com>
2025-03-20 14:29:58 -0400

    update for whitespace in IFS

--- fp.bash
+++ fp.bash
@@ -4,16 +4,25 @@
 #
 # All function and variable names are camelCased, but they may begin with uppercase letters.
 #
-# Function names are prefixed with "task." (always lowercase) so they are namespaced.
-# Keyword function names are the exception to this.
-# They are all lowercase letters and attempt to be five letters or shorter.
+# Function names are prefixed with "fp." (always lowercase) so they are namespaced.
 #
 # Local variable names begin with lowercase letters.
 # Global variable names begin with uppercase letters.
-# Global variable names are namespaced by suffixing them with the randomly-generated letter C.
+# Global variable names are namespaced by suffixing them with the randomly-generated letter F.
 #
 # Private function names begin with lowercase letters.
 # Public function names begin with uppercase letters.
+
+# fp.Collect collects a stream into a string.
+fp.Collect() {
+  local sep=${1:-${IFS:0:1}}
+
+  local IFS=$sep field results=()
+  while IFS='' read -r field; do
+    eval "results+=( $field )"
+  done
+  echo "${results[*]}"
+}
 
 # fp.Each applies its arguments as a command to each argument from stdin.
 fp.Each() {
@@ -44,11 +53,29 @@ fp.Map() {
   done
 }
 
-# fp.Stream echoes arguments escaped and separated by newline.
+# fp.RemoveIf filters lines from stdin using the negation of command.
+fp.RemoveIf() {
+  local command=$1 arg
+  while IFS='' read -r arg; do
+    eval "! $command $arg" && echo "$arg"
+  done
+}
+
+# fp.Stream echoes arguments escaped and separated by the first character of IFS.
 fp.Stream() {
   local arg
-  for arg in $*; do
-    printf '%q\n' "$arg"
+  for arg in "$@"; do
+    printf "%q${IFS:0:1}" "$arg"
+  done
+}
+
+# fp.StreamList echoes list, splitting on sep.
+fp.StreamList() {
+  local list=$1 sep=${2:-${IFS:0:1}}
+
+  local IFS=$sep
+  for field in $list; do
+    printf '%q\n' $field
   done
 }
 
