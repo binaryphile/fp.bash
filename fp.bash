4854665e7d0d0f223e80bbb377de23ed7cc8769a
Ted Lilley <ted.lilley@digi.com>
2025-03-20 10:18:01 -0400

    fix comments

--- fp.bash
+++ fp.bash
@@ -1,5 +1,20 @@
 # fp.bash -- functional programming in bash
 
+# Naming Policy:
+#
+# All function and variable names are camelCased, but they may begin with uppercase letters.
+#
+# Function names are prefixed with "task." (always lowercase) so they are namespaced.
+# Keyword function names are the exception to this.
+# They are all lowercase letters and attempt to be five letters or shorter.
+#
+# Local variable names begin with lowercase letters.
+# Global variable names begin with uppercase letters.
+# Global variable names are namespaced by suffixing them with the randomly-generated letter C.
+#
+# Private function names begin with lowercase letters.
+# Public function names begin with uppercase letters.
+
 # fp.Each applies its arguments as a command to each argument from stdin.
 fp.Each() {
   local command=$1 arg
@@ -16,15 +31,16 @@ fp.KeepIf() {
   done
 }
 
-# fp.Map returns $expression evaluated with the value of stdin as $varname.
-# $expression must respect double-quoting rules and so can't contain naked quotes.
-# $varname may not be "varname" or "expression".
+# fp.Map returns $EXPRESSION evaluated with the value of stdin as $VARNAME.
+# $EXPRESSION must respect double-quoting rules and so can't contain naked quotes.
+# $VARNAME may not be "VARNAME" or "EXPRESSION".
 fp.Map() {
-  local VarnameC=$1 ExpressionC=$2
-  case $VarnameC in VarnameC|ExpressionC ) fp.fatal "fp.Map: VarnameC may not be 'VarnameC' or 'ExpressionC'";; esac
-  local $VarnameC
-  while IFS='' read -r "$VarnameC"; do
-    eval "echo \"$ExpressionC\""
+  local VARNAME EXPRESSION  # borrow a different namespace since we're passing a variable name
+  case $VARNAME in VARNAME|EXPRESSION ) fp.fatal "fp.Map: VARNAME may not be 'VARNAME' or 'EXPRESSION'";; esac
+
+  local "$VARNAME"
+  while IFS='' read -r "$VARNAME"; do
+    eval "echo \"$EXPRESSION\""
   done
 }
 
